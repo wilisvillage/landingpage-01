a26887428367209b1e0463e3ff6be2acf3f24e42
fairuzmk <fairuzdeveloper@gmail.com>
2025-06-20 19:33:01 +0700

    0.0.1

--- template/js/main.js
+++ template/js/main.js
@@ -122,7 +122,98 @@ jQuery(document).ready(function($) {
 	};
 	siteSliderRange();
 
+	var carousel = function() {
+		$('.home-slider').owlCarousel({
+	    loop:true,
+	    autoplay: true,
+	    margin:0,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav:false,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:1
+	      },
+	      1000:{
+	        items:1
+	      }
+	    }
+		});
+		$('.carousel-properties').owlCarousel({
+			autoplay: true,
+			center: false,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			dots: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
+		$('.carousel-agent').owlCarousel({
+			autoplay: true,
+			center: false,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
+		$('.carousel-testimony').owlCarousel({
+			autoplay: true,
+			autoHeight: true,
+			center: true,
+			loop: true,
+			items:1,
+			margin: 30,
+			stagePadding: 0,
+			nav: false,
+			dots: true,
+			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+			responsive:{
+				0:{
+					items: 1
+				},
+				600:{
+					items: 2
+				},
+				1000:{
+					items: 3
+				}
+			}
+		});
 
+	};
+	carousel();
 	
 	var siteCarousel = function () {
 		if ( $('.nonloop-block-13').length > 0 ) {
@@ -273,8 +364,10 @@ jQuery(document).ready(function($) {
 
   		if (st > 100) {
   			$('.js-sticky-header').addClass('shrink');
+			$('#logo-wilis').attr('src', 'images/logowilis.png'); // hitam
   		} else {
   			$('.js-sticky-header').removeClass('shrink');
+			$('#logo-wilis').attr('src', 'images/logowiliswhite.png'); // putih
   		}
 
   	}) 
